10229d0c85b94caa96e536fc28e55864cbbba515
Lesley <leskimuti@gmail.com>
2023-04-15 13:57:21 +0300

    Adding overlay and disabling scroll on mobile nav open

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,11 @@
 const menu = document.querySelector(".hamburger");
 const navlinks = document.querySelector(".nav--list");
+const body = document.querySelector("body");
+const overlay = document.querySelector("#overlay");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("open");
   navlinks.classList.toggle("addnav");
+  body.classList.toggle("lock-scroll");
+  overlay.classList.toggle("overlay-on");
 });
